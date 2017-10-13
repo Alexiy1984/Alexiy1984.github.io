c441a6b5dafdf65a74652e53ac5ea3b77082a61d
Alexiy1984 <alevaliev@yandex.ru>
2017-10-13 17:27:23 +0300

    Commit 30
    Продолжение работы с JQuery

--- JavaScript (jQuery)/step5/js/myscript.js
+++ JavaScript (jQuery)/step5/js/myscript.js
@@ -1,21 +1,45 @@
 $(document).ready(function () {
 
-	$('.whilebutt').click(function () {
-   
-		var i = 0;
+	var mass = ['дом', 'сад', 'лес', 'дар', 'ром','мор', 'гор', 'двор', 'упор', 'кор', 'дол', 'рот', 'год', 'сом', 'нос'];
+	var mass2 = [];
 
-		while	(i < 10) {
-
-			i ++;
-			$('#row').append('i = ' + i + '<br>');
-
-
+	$('.whilebutt').click(function() {
+   	var i = 0;
+		
+		while	(i < mass.length) {
+			
+			$('#row').append('<p class="text-center"> ' + (i + 1) + ' Элемент массива = ' + mass[i] + ' (while)</p>');
+			i ++;	
 		}
 
-
-
-
+		$('#row').append('<p class="text-center"> Размер массива ' + i + ' элементов </p>');
 	});
 	
+	$('.forbutt').click(function () {
+   	
+		for (var i = 0; i < mass .length; i++) {
+			$('#row').append('<p class="text-center"> ' + (i + 1) + ' Элемент массива = ' + mass[i] + ' (for)</p>');
+		}
+
+		$('#row').append('<p class="text-center"> Размер массива ' + i + ' элементов </p>');
+	});
+
+	$('.dowbutt').click(function () {
+   	
+   	do  {
+
+			var number = prompt('Введите размер нового массива (число меньше 15)')
+
+		} while(isNaN(number) || number > 15);
+
+		
+		for (var i = 0; i < number; i++) {
+			mass2[i] = mass[i];
+			$('#row').append('<p class="text-center"> ' + (i + 1) + ' Элемент нового массива = ' + mass2[i] + ' </p>');
+		}
+
+		$('#row').append('<p class="text-center"> Размер нового массива ' + i + ' элементов </p>');
+
+	});
 	
 });
--- JavaScript (jQuery)/step6/js/myscript.js
+++ JavaScript (jQuery)/step6/js/myscript.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
 
+
+	function newfun() {
+
+		var today = new Date();
+		
+
+		$('.content').append('<p class="text-center"> Сегодня ' + today.toDateString()+ ' </p>');
+		$('.content').append('<p class="text-center"> Сегодня ' + today.toString() + ' </p>');
+	}
 	
-	
+	$('#short').click(function () {
+		newfun();
+	});  
+
 });
