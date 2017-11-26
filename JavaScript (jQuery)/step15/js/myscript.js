48e3c9e408faf7018b127bbd323a169c37c5a110
Alexiy1984 <alevaliev@yandex.ru>
2017-11-26 16:10:45 +0300

    Commit 55
    Добавлена часть контента в Работу со ссылками  JQuery

--- JavaScript (jQuery)/step15/js/myscript.js
+++ JavaScript (jQuery)/step15/js/myscript.js
@@ -1,4 +1,14 @@
 $(document).ready( function () {
- 
+ $('#link-processing').click(function () {
+ 	$('#linkforpr a[href^="http://"], #linkforpr a[href^="https://"]').each(function () {
+ 		var href = $(this).attr('href');
+ 		href = href.replace('http://','');
+ 		href = href.replace('https://','');
+ 		$(this).append(' ('+ href +')');
+ 	});
+ 	$(this).attr("disabled", true)
+ });
+var myURL = location.protocol + '//' + location.hostname;
+$('#linkfortar a[href^="http://"], #linkfortar a[href^="https://"]').not('[href^="'+myURL+'"]').attr('target' , '_blank');
 
 });
