6659bd80da6fd752194513a38487c6053dca726d
Alexiy1984 <alevaliev@yandex.ru>
2017-12-14 22:25:24 +0300

    Commit 59
    Поработал над Улучшением веб форм

--- JavaScript (jQuery)/step18/js/myscript.js
+++ JavaScript (jQuery)/step18/js/myscript.js
@@ -4,15 +4,36 @@ $(document).ready( function () {
 
 	$('.numonly').blur(function () {
 		var FildValue = $(this).val();
+		var PriceInd = $('#price').val();
+		var QuantityInd = $('#quantity').val();
 		if (typeof(FildValue)!=='number'){
 			if (isNaN(FildValue)) {
 				$(this).addClass('error-marker');
 				alert('Пожалуйста введите число');
+				$('#sum').val('');
 			}
 			else {
 				$(this).removeClass('error-marker');
+				if (typeof(PriceInd)!=='number' && typeof(QuantityInd)!=='number') {
+					$('#sum').val(PriceInd * QuantityInd);
+					
+				}	else {$('#sum').val('');}
 			}
 		}	
 	} );
+
+	$('.newform').submit(function (evt) {
+		if ($('#FullName').val()=='') {
+			alert('Введите пожалуйста имя в поле ' + $('label[for="FullName"]').text());
+			return false;
+			/*evt.preventDefault();*/	
+		}
+		if ($('#Email').val()=='') {
+			alert('Введите пожалуйста имя в поле ' + $('label[for="Email"]').text());
+			return false;
+			/*evt.preventDefault();*/	
+		}
+	});
+
 
 });
