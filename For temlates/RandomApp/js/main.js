5cf0e738526fd7ddd11bd7602145c0aecb5f5b57
Alexiy1984 <31098105+Alexiy1984@users.noreply.github.com>
2017-12-27 18:03:25 +0300

    Commit 65
    Правка анимации

--- For temlates/RandomApp/js/main.js
+++ For temlates/RandomApp/js/main.js
@@ -4,21 +4,21 @@ $(document).ready(function () {
 		h2bigsize = +h2size.replace('px','')+5;
 		p2size = $('.col-2 p',this).css('font-size');
 		p2bigsize = +p2size.replace('px','')+5;
-		$('.col-1 img',this).animate({
+		$('.col-1 img',this).stop().animate({
 			filter: 'none', 
 			'box-shadow': '0 0 50px 2px rgba(255,255,255,0.25)'
 		},'fast');
-		$('.col-2 h2',this).animate({
+		$('.col-2 h2',this).stop().animate({
 			'font-size': h2bigsize+'px'
 		}, 400);
-		$('.col-2 p',this).animate({
+		$('.col-2 p',this).stop().animate({
 			'font-size': p2bigsize+'px'
 		}, 400);
 	}, function () {
-		$('.col-2 h2',this).animate({
+		$('.col-2 h2',this).stop().animate({
 			'font-size': h2size
 		}, 400);
-		$('.col-2 p',this).animate({
+		$('.col-2 p',this).stop().animate({
 			'font-size': p2size
 		}, 400);
 	}
