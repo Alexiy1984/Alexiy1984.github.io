98ec1428fe6a4e0a32338b2bf6dfa40c7cf95705
Alexiy1984 <alevaliev@yandex.ru>
2018-02-02 22:52:18 +0300

    Шаблон Title (v.0.7.5)
    что сделано:
    -отработаны размеры отступов и шрифтов в дополнительных блоках
    -добавлен эффект появления/исчезновения блоков
    
    что надо сделать:
    -разобраться с эффектом появления фона
    -добить цвета/толщину шрифтов в дополнительных блоков

--- For templates/Title/js/hide_show.js
+++ For templates/Title/js/hide_show.js
@@ -1,5 +1,49 @@
 window.onload = function () {
 
+	let sheet = (function() {
+	let style = document.createElement("style");
+	style.appendChild(document.createTextNode(""));
+	document.head.appendChild(style);
+	return style.sheet;
+	})();
+
+	let firstRule = "body:before {" +
+																"z-index: 1;" + 
+																"content: '';" +
+																"position: fixed;" +
+																"top:0;" +
+																"left: 0;" +
+																"width: 100vw;" +
+																"height: 100vh;" +
+																"background: linear-gradient(45deg, rgba(191,117,135,0.639) 0%, rgba(214,255,255,0.631) 100%), url();" +
+																"background-size: cover, cover;" +
+																"background-position: 0 0, center;" +
+																"background-repeat: repeat, no-repeat;" +
+																"transition: background-image 3s;}"; 
+	let secondRule = "body:before {" +
+																"z-index: 1;" + 
+																"content: '';" +
+																"position: fixed;" +
+																"top:0;" +
+																"left: 0;" +
+																"width: 100vw;" +
+																"height: 100vh;" +
+																"background: linear-gradient(45deg, rgba(191,117,135,0.639) 0%, rgba(214,255,255,0.631) 100%), url('./images/bg(1).jpg');" +
+																"background-size: cover, cover;" +
+																"background-position: 0 0, center;" +
+																"background-repeat: repeat, no-repeat;" +
+																"transition: background-image 3s;}"; 
+
+	sheet.insertRule(firstRule, 0);
+
+	setTimeout(addimg, 3000);
+
+	function addimg() {
+		sheet.insertRule(secondRule, 1);
+	}
+
+	console.log(document.styleSheets[1]);
+
 	let buttarr = document.getElementsByClassName('buttons');
 	
 	[].forEach.call(buttarr, function(arrel) {
@@ -29,13 +73,24 @@ window.onload = function () {
 			for ( i = 1; i <= 3; ++i) {
 				let HideblockN = document.getElementById('addit-content'+ i);
 				HideblockN.style.display = 'none';
+				HideblockN.style.opacity = 0;
 			}
 			blockToShow.style.display = 'block';
+			opacity(blockToShow, 10);
 		} else {
 			let blockToHide = document.getElementById(elemId);
 			blockToHide.style.display = 'none'; 
-			blockToShow.style.display = 'block';
+			blockToHide.style.opacity = 0; 
+			blockToShow.style.display = 'block';	
+			opacity(blockToShow, 10);
 		}
+	}
+
+	function opacity(element, speed){
+    let interv = setInterval(function(){    
+    	if (element.style.opacity > 1) clearInterval(interv);
+       element.style.opacity = +element.style.opacity + 0.01;
+    }, speed)
 	}
 
 }
