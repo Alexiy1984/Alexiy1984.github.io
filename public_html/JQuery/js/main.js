2498d06c6b3376049eb486b113b52116fe683511
Alexiy1984 <alevaliev@yandex.ru>
2018-02-07 21:50:43 +0300

    Технический коммит
    -добавлено немного кода в раздел JQuery
    -добавлены вендорые префиксы

--- public_html/JQuery/js/main.js
+++ public_html/JQuery/js/main.js
@@ -3,10 +3,16 @@
 })*/
 $(function() {
 	$('#first').focus();
-	$('input').focus(function() {
+	$('input[type="text"]').focus(function() {
 		$(this).css({'background':'#9CA', 'border':'solid 1px green' });
 	});
-	$('input').blur(function() {
+	$('input[type="text"]').blur(function() {
 		$(this).css({'background':'#D99','border':'solid 1px red' });
 	});
+	$('input[type="button"]').click(function() {
+		$(this).slideUp();
+	})
+  $('input[type="button"]').dblclick(function() {
+		$(this).hide();
+	})
 })
