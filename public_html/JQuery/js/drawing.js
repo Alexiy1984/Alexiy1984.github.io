31a207434a48e020f8b7e9237340ae17144e343f
Alexiy1984 <alevaliev@yandex.ru>
2018-02-09 18:42:46 +0300

    Технический коммит

--- public_html/JQuery/js/drawing.js
+++ public_html/JQuery/js/drawing.js
@@ -1,13 +1,13 @@
 $(function() {
 	let canvas = $('#pad')[0];
-	let inner = $('.inner')[0];
-	let container = $('.container')[0]; 
 	let context = canvas.getContext("2d");
 	let pendown = false;
 	
 	$('#pad').mousemove(function(event) {
 		let xpos = event.pageX - (canvas.offsetLeft);
+		console.log(xpos+":"+canvas.offsetLeft);
 		let ypos = event.pageY - (canvas.offsetTop);
+		console.log(ypos);
 	
 		if (pendown) context.lineTo(xpos, ypos)
 		else context.moveTo(xpos, ypos);
