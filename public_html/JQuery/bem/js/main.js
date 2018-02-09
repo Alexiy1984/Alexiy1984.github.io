7969f1e8b9d52f9f92ff46f3fa434b876c676955
Alexiy1984 <alevaliev@yandex.ru>
2018-02-09 21:28:23 +0300

    Проработан раздел по JQuery
    -добавлено ещё немного кода JQuery
    -немного поправлен css

--- public_html/JQuery/bem/js/main.js
+++ public_html/JQuery/bem/js/main.js
@@ -16,7 +16,7 @@ $(function() {
 		});
 	};
 	function listParentStyle(listStyle) {
-		$('li').parent().css('font-weight', 'bold');
+		$('li').parent().css({'font-weight':'400', 'font-size': '0.75em'});
 		$('li').parent('.list-style').css('list-style-type', listStyle);
 	}
 	function Parents() {
@@ -25,6 +25,32 @@ $(function() {
 	function ParentsUntil() {
 		$('li').parentsUntil('div').css('background', 'red');
 	}
+
+	$('.board').children('.board__cell_back').addClass('bg-black');
+
+	
+	function cellColorChange() {
+		$('.board__cell_back').toggleClass('bg-black');
+		$('.board__cell_white').toggleClass('bg-black');
+	}
+
+	$('.board__cell').mouseover(function() {
+		$(this).attr('title','нажми меня');
+	});
+	$('.board__cell').click(function() {
+		cellColorChange();
+	});
+
+	$('.listdiv__ul li').css('cursor', 'pointer');
+	$('.listdiv__ul li').click(function() {
+			findNext(this);
+	})
+
+	function findNext(elem) {
+		$('.listdiv__ul li').css('color', 'black');
+		$(elem).next().css('color', 'red');
+	}
+	
 	/*Parents();
 	ParentsUntil();*/
 	listParentStyle('square');
