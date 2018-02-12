3f2692e5fb6190afd84c4615d7095e05e15beaaf
Alexiy1984 <alevaliev@yandex.ru>
2018-02-12 21:45:08 +0300

    Шаблон City (v.0.8.5)
    что сделано:
    
    -переделана логика изменения размера циферблата (вместо внутренних отступов
    изменяются размеры элемента)
    -добавлена css анимация (пока есть неприятынй баг с перезапуском анимации,
    происходит после смены позиции body:before)
    -добавлен минифиыированный css

--- For templates/City/js/main.js
+++ For templates/City/js/main.js
@@ -2,23 +2,23 @@ let remain_bv   = 15*60*60*24;
 
 function parseTime_bv(timestamp){
   if (timestamp < 0) timestamp = 0;
- 
+
   let day = Math.floor( (timestamp/60/60) / 24);
   let hour = Math.floor(timestamp/60/60);
   let min = Math.floor((timestamp - hour*60*60)/60);
-  let sec = Math.floor(timestamp - hour*60*60 - min*60); 
+  let sec = Math.floor(timestamp - hour*60*60 - min*60);
   let left_hour = Math.floor( (timestamp - day*24*60*60) / 60 / 60 );
 
   let days = document.getElementById('days');
   let hours = document.getElementById('hours');
   let mins = document.getElementById('mins');
   let secs = document.getElementById('secs');
- 
+
   days.innerHTML = day;
   if(String(left_hour).length > 1)
   	hours.innerHTML = left_hour;
 	else
-		hours.innerHTML = "0" + left_hour; 
+		hours.innerHTML = "0" + left_hour;
   if(String(min).length > 1)
       mins.innerHTML = min;
    else
@@ -26,15 +26,15 @@ function parseTime_bv(timestamp){
    if(String(sec).length > 1)
       secs.innerHTML = sec;
    else
-      secs.innerHTML = "0" + sec;   
-};	
+      secs.innerHTML = "0" + sec;
+};
 
 window.onload = function () {
 	setInterval(function(){
     remain_bv = remain_bv - 1;
     parseTime_bv(remain_bv);
     	if(remain_bv <= 0){
-          
+
     	}
     }, 1000);
 }
