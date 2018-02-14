8f1cb5956b595c150a70cd535b25a49c5660c3e4
Alexiy1984 <alevaliev@yandex.ru>
2018-02-14 12:18:56 +0300

    HTML5 Video audio
    Технический коммит TBC...

--- public_html/HTML5/js/audioVideoPlay.js
+++ public_html/HTML5/js/audioVideoPlay.js
@@ -13,10 +13,9 @@ window.onload = function () {
     O('myvideo').pause();
   }
 
-  O('js_playaudio').addEventListener("click", playaudio());
-  O('js_pauseaudio').addEventListener("click", pauseaudio());
-  O('js_playvideo').addEventListener("click", playvideo());
-  O('js_pausevideo').addEventListener("click", pausevideo());
-
+  O('js_playaudio').addEventListener("click", playaudio);
+  O('js_pauseaudio').addEventListener("click", pauseaudio);
+  O('js_playvideo').addEventListener("click", playvideo);
+  O('js_pausevideo').addEventListener("click", pausevideo);
 
 }
