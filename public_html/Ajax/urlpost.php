dd7947f486087a6fed1bab71036b17353fd03f27
AlexiyV <alevaliev@yandex.ru>
2018-02-22 20:55:49 +0300

    Технический коммит после переустановки системы (провека работы git и настройки ST3)

--- public_html/Ajax/urlpost.php
+++ public_html/Ajax/urlpost.php
@@ -1,11 +1,11 @@
-<?php 
-	if (isset($_POST['url'])) {
-		echo file_get_contents("http://".SanitizeString($_POST['url']));
-	}
+<?php
+  if (isset($_POST['url'])) {
+    echo file_get_contents("http://".SanitizeString($_POST['url']));
+  }
 
-	function SanitizeString($var) {
-		$var = strip_tags($var);
-		$var = htmlentities($var);
-		return stripslashes($var);
-	}
+  function SanitizeString($var) {
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    return stripslashes($var);
+  }
 ?>
